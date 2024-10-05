727cd8e18489b23e1bc0d79b5802025afdc160ff
USER <mas9n9-mas9n91332@yandex.ru>
2024-10-05 10:41:30 +0500

    Completed all the tasks!

--- src/Main.java
+++ src/Main.java
@@ -26,12 +26,12 @@ public class Main {
         //task3
         System.out.println("Task 3");
         double[] payments2 = {12500, 10000, 9550, 12000, 99000};
-        int totalExpense = 0;
+        double totalExpense = 0;
         for (double vyplota2 : payments2) {
             totalExpense += vyplota2;
         }
-        int weeksInMonth = 4;
-        double averageExpense = (double) totalExpense / weeksInMonth;
+        int numberOfPayments = payments2.length;
+        double averageExpense = totalExpense / numberOfPayments;
         System.out.printf("Средняя сумма трат за месяц составила %.2f рублей.\n", averageExpense);
         //task 4
         System.out.println("Task 4");
